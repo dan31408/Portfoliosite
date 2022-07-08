08d45b9f27a429f36b41cd709350ecc8b912753b
Dan <dan.beto31408@gmail.com>
2022-07-08 11:04:05 -0500

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import Dan from './app/assets/img/Dan.jpg';
 import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
-      Hello Dan!
+      <Navbar dark color='primary' sticky='top' expand='md' align='left'>
+               <Container>
+                    <NavbarBrand href='/'>
+                        <img src={Dan} alt='Portfolio Image' />
+                    </NavbarBrand>
+                </Container>
+            </Navbar>
+            I'm Ready!
     </div>
   );
 }
